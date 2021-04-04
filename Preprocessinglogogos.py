af8f8c5e12d9cde89c32b1c13c49d2a6d1ed8e13
AeroMarco <marcoperaltatapia@gmail.com>
2021-04-04 08:32:42 +0200

    preplanning almost done

--- Preprocessinglogogos.py
+++ Preprocessinglogogos.py
@@ -8,20 +8,38 @@ import numpy as np
 import codecs
 import imagefuncts as imf
 import math
+import matplotlib
+
 
 
 
 def preprocessing_logos():  #this function does all the preprocessing for plane logo recognition. It retuns a list of vectors corresponding to the pictures in order form 1 to 100.
-    
+    #this program will find the top 2000 colors in rgb format
+
     n = 1
-    n_pictures = 1 #number of pictures in dataset
+    n_pictures = 10 #number of pictures in dataset
     lengthlist = []
     vectorlist = []
     while n <= n_pictures: #for every picture
-        
+
+        lengthlist = []
         img = Image.open(str(n)+'.jpg')
         new_img = img.resize((400,300))
-        arra = np.array(new_img)
+        im_rgb = img.convert('RGB')
+        colors = im_rgb.getcolors(maxcolors=200000)  
+        colors = sorted(colors, key = lambda x:-x[0])
+        for a in range (2000):
+            col = matplotlib.colors.to_hex([ colors[a][1][0]/255, colors[a][1][1]/255, colors[a][1][2]/255 ])
+            lengthlist.append(col)
+        #print(lengthlist)
+    
+
+        n = n +1
+    vectorlist.append(lengthlist)
+
+
+    return vectorlist
+
 
 def Write_up(vect):
     f= open("PreplanningCarrier.txt","w+")
@@ -33,3 +51,5 @@ def Write_up(vect):
     f.close()
     return 
 
+
+Write_up(preprocessing_logos())
